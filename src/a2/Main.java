6d61fc8f9a49fc23d6ab0e3e94f76a0c293c806c
Sirine Dridi <sirine@live.unc.edu>
2021-02-08 12:39:09 -0500

    fixed main

--- src/a2/Main.java
+++ src/a2/Main.java
@@ -4,7 +4,7 @@ package a2;
 public class Main {
 
     public static void main(String[] args) {
-        LinkedList list = new LinkedList<Integer>();
+        LinkedList list = new LinkedList();
         list.add(1);
         list.add(2);
         list.add(3);
